b8ee61edd5b3b2b786d37f8710cb9d09cc1e1124
leninner <mazabandalenin180@gmail.com>
2021-08-12 17:51:45 -0500

    Añado refactorización de diseño

--- form.js
+++ form.js
@@ -1,11 +1,11 @@
 // There are many ways to pick a DOM node; here we get the form itself and the email
 // input box, as well as the span element into which we will place the error message.
-const form = document.getElementsByTagName("form")[0];
+const form = document.getElementsByTagName("form")[1];
 
 const email = document.getElementById("mail");
 const emailError = document.querySelector("#mail + span.error");
 
-email.addEventListener("input", function (event) {
+email.addEventListener("input", function () {
     // Each time the user types something, we check if the
     // form fields are valid.
 
