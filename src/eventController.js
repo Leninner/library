540b3bb23326b802f7201faf2e0870820aeaa301
leninner <mazabandalenin180@gmail.com>
2021-08-20 23:18:45 -0500

    Añado estilos

--- src/eventController.js
+++ src/eventController.js
@@ -5,8 +5,14 @@ const len = (button) => {
 const displayForm = () => {
     const formulario = document.querySelector(".form");
     if (!formulario.innerHTML) {
-        formulario.innerHTML = `Lenin`;
+        formulario.classList.add("active");
+        formulario.innerHTML = `<h2>Book</h2>
+        <label for="title">Title: <input type="text" id="title" placeholder="Title" /></label>
+        <label for="autor">Author: <input type="text" id="autor" placeholder="Author" /></label>
+        <label for="pages"># Pages: <input type="number" id="pages" placeholder="Number of pages" /></label>
+        <label for="year">Year: <input type="date" id="year" placeholder="Year of Publication" /></label>`;
     } else {
+        formulario.classList.remove("active");
         formulario.innerHTML = ``;
     }
 };
