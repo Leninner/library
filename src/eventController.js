bf3b9a145a32498f5861a751701327f313820178
leninner <mazabandalenin180@gmail.com>
2021-08-25 20:54:27 -0500

    Añado diseño

--- src/eventController.js
+++ src/eventController.js
@@ -1,20 +1,14 @@
 const len = (button) => {
-    button.addEventListener("click", displayForm);
+  button.addEventListener('click', displayForm);
 };
 
 const displayForm = () => {
-    const formulario = document.querySelector(".form");
-    if (!formulario.innerHTML) {
-        formulario.classList.add("active");
-        formulario.innerHTML = `<h2>Book</h2>
-        <label for="title">Title: <input type="text" id="title" placeholder="Title" /></label>
-        <label for="autor">Author: <input type="text" id="autor" placeholder="Author" /></label>
-        <label for="pages"># Pages: <input type="number" id="pages" placeholder="Number of pages" /></label>
-        <label for="year">Year: <input type="date" id="year" placeholder="Year of Publication" /></label>`;
-    } else {
-        formulario.classList.remove("active");
-        formulario.innerHTML = ``;
-    }
+  const formulario = document.querySelector('.form');
+  if (!formulario.classList.contains('active')) {
+    formulario.classList.add('active');
+  } else {
+    formulario.classList.remove('active');
+  }
 };
 
 export default len;
