73325126a3243f117c96a2a82d4362824abdcc00
leninner <mazabandalenin180@gmail.com>
2021-08-27 21:20:58 -0500

    Update

--- src/eventController.js
+++ src/eventController.js
@@ -1,3 +1,5 @@
+import { Book } from './library.js'; // Constructor
+
 const len = (button) => {
   button.addEventListener('click', displayForm);
 };
@@ -6,6 +8,18 @@ const displayForm = () => {
   const formulario = document.querySelector('.form');
   if (!formulario.classList.contains('active')) {
     formulario.classList.add('active');
+    const addBookButton = document.querySelector('.addBook');
+    addBookButton.addEventListener('click', () => {
+      formulario.classList.remove('active');
+      const title = document.querySelector('#title'),
+        autor = document.querySelector('#autor'),
+        pages = document.querySelector('#pages'),
+        year = document.querySelector('#year');
+
+      const book = new Book(title.value, autor.value, pages.value, year.value, true);
+      book.setInfo();
+      book = null;
+    });
   } else {
     formulario.classList.remove('active');
   }
--- src/library.js
+++ src/library.js
@@ -1,34 +1,34 @@
 // Clase para crear libros
 
 class Book {
-    constructor(title, autor, pages, year, estado) {
-        this.title = title;
-        this.autor = autor;
-        this.pages = pages;
-        this.year = year;
-        this.estado = estado;
+  constructor(title, autor, pages, year, estado) {
+    this.title = title;
+    this.autor = autor;
+    this.pages = pages;
+    this.year = year;
+    this.estado = estado;
 
-        if (!books[title]) {
-            books[title] = {
-                title: this.title,
-                autor: this.autor,
-                year: this.year,
-                pages: this.pages,
-                estado: this.estado,
-            };
-            console.log(books);
-        } else {
-            alert("Ese libro ya existe");
-        }
+    if (!books[title]) {
+      books[title] = {
+        title: this.title,
+        autor: this.autor,
+        year: this.year,
+        pages: this.pages,
+        estado: this.estado,
+      };
+      console.log(books);
+    } else {
+      alert('Ese libro ya existe');
     }
+  }
 
-    setInfo() {
-        const carousel__container = document.querySelector(".carousel__container");
-        const carousel__item = document.createElement("div");
+  setInfo() {
+    const carousel__container = document.querySelector('.carousel__container');
+    const carousel__item = document.createElement('div');
 
-        carousel__item.classList.add("carousel-item");
+    carousel__item.classList.add('carousel-item');
 
-        carousel__item.innerHTML = `
+    carousel__item.innerHTML = `
             <img class="carousel-item__img"
             src="https://images.pexels.com/photos/4629625/pexels-photo-4629625.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
             alt="${this.title}"
@@ -42,18 +42,18 @@ class Book {
                 </div>
             </div>`;
 
-        carousel__container.appendChild(carousel__item);
-    }
+    carousel__container.appendChild(carousel__item);
+  }
 }
 
 let books = {
-    "La Cabaña del Tío Tom": {
-        title: "La Cabaña del Tío Tom",
-        autor: "Leninner",
-        year: 1784,
-        pages: 4850,
-        estado: true,
-    },
+  'La Cabaña del Tío Tom': {
+    title: 'La Cabaña del Tío Tom',
+    autor: 'Leninner',
+    year: 1784,
+    pages: 4850,
+    estado: true,
+  },
 };
 
 export { books, Book };
